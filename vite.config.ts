92a2a5931d19a755c76c970acbad4f6c165743d0
Siim Alve <siim.alve@gmail.com>
2026-04-29 22:33:36 +0300

    asdf

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,13 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/setomaa-youth-connect/',
+  
   server: {
     host: "::",
     port: 8080,
     hmr: {
-      overlay: false,
-      base: '/setomaa-youth-connect/'
+      overlay: false
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
